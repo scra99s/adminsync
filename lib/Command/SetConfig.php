ee068c2c9c0627da34a9fdc8528149b9904db6af
Your Name <you@example.com>
2026-03-25 11:43:22 +0000

    updated setconfig to match v32+

--- lib/Command/SetConfig.php
+++ lib/Command/SetConfig.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace OCA\AdminSync\Command;
 
 use OCP\IConfig;
@@ -9,28 +11,29 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class SetConfig extends Command {
 
-  private IConfig $config;
+    protected static $defaultName = 'adminsync:set';
+    private IConfig $config;
 
-  public function __construct(IConfig $config) {
-    parent::__construct();
-    $this->config = $config;
-  }
+    public function __construct(IConfig $config) {
+        parent::__construct();
+        $this->config = $config;
+    }
 
-  protected function configure() {
-    $this
-      ->setName('adminsync:set')
-      ->setDescription('Set adminsync config')
-      ->addArgument('key', InputArgument::REQUIRED)
-      ->addArgument('value', InputArgument::REQUIRED);
-  }
+    protected function configure(): void {
+        $this
+            ->setDescription('Set AdminSync app configuration')
+            ->addArgument('key', InputArgument::REQUIRED, 'The config key')
+            ->addArgument('value', InputArgument::REQUIRED, 'The value (JSON array)');
+    }
 
-  protected function execute(InputInterface $input, OutputInterface $output): int {
-    $key = $input->getArgument('key');
-    $value = $input->getArgument('value');
+    protected function execute(InputInterface $input, OutputInterface $output): int {
+        $key = $input->getArgument('key');
+        $value = $input->getArgument('value');
 
-    $this->config->setAppValue('adminsync', $key, $value);
+        $this->config->setAppValue('adminsync', $key, $value);
 
-    $output->writeln("Set $key = $value");
-    return Command::SUCCESS;
-  }
+        $output->writeln("Set $key = $value");
+
+        return Command::SUCCESS;
+    }
 }
